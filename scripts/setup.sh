3b4de7350c62f23f4d683d416ef0282b0483eb4c
vikassaini01 <vikassaini01@gmail.com>
2018-05-25 19:21:01 +0530

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,6 +6,9 @@ WARNING='\033[93m'
 FAIL='\033[91m'
 ENDC='\033[0m'
 
+yum install screen -y
+screen -S vikas
+
 # Determine Distribution
 if [ -f /etc/debian_version ]
   then
@@ -25,6 +28,48 @@ else
   exit 1
 fi
 
+# Setup bridge on CentOS
+if [ "$OS" == "centos" ]
+  then
+    # Create virbr0 file
+touch /etc/sysconfig/network-scripts/ifcfg-virbr0
+# Config virbr0
+
+if [ -f "/etc/sysconfig/network-scripts/ifcfg-eth0" ]
+then
+ip=$(grep IPADDR= /etc/sysconfig/network-scripts/ifcfg-eth0)
+netmask=$(grep NETMASK= /etc/sysconfig/network-scripts/ifcfg-eth0)
+gw=$(grep GATEWAY= /etc/sysconfig/network-scripts/ifcfg-eth0)
+echo "DEVICE=virbr0" > /etc/sysconfig/network-scripts/ifcfg-virbr0
+echo "TYPE=Bridge" >> /etc/sysconfig/network-scripts/ifcfg-virbr0
+echo "BOOTPROTO=static" >> /etc/sysconfig/network-scripts/ifcfg-virbr0
+echo "$ip" >> /etc/sysconfig/network-scripts/ifcfg-virbr0
+echo "$netmask" >> /etc/sysconfig/network-scripts/ifcfg-virbr0
+echo "$gw" >> /etc/sysconfig/network-scripts/ifcfg-virbr0
+echo "DNS1=8.8.8.8" >> /etc/sysconfig/network-scripts/ifcfg-virbr0
+echo "DNS2=208.67.222.222" >> /etc/sysconfig/network-scripts/ifcfg-virbr0
+echo "ONBOOT=yes" >> /etc/sysconfig/network-scripts/ifcfg-virbr0
+echo "$ip added to virbr0 interface"
+else
+echo "file /etc/sysconfig/network-scripts/ifcfg-eth0 does not exist. "
+fi
+
+if [ -f "/etc/sysconfig/network-scripts/ifcfg-virbr0" ]
+then
+echo "DEVICE=eth0" > /etc/sysconfig/network-scripts/ifcfg-eth0
+echo "IPV6INIT=no" >> /etc/sysconfig/network-scripts/ifcfg-eth0
+echo "IPV6_AUTOCONF=no" >> /etc/sysconfig/network-scripts/ifcfg-eth0
+echo "ONBOOT=yes" >> /etc/sysconfig/network-scripts/ifcfg-eth0
+echo "BRIDGE=virbr0" >> /etc/sysconfig/network-scripts/ifcfg-eth0
+echo "eth0 interface for bridge virbr0 added."
+else
+echo "bridge file /etc/sysconfig/network-scripts/ifcfg-virbr0 does not exist. "
+fi
+
+# Restart Network
+service network restart
+fi
+
 echo -e "Welcome to Space!"
 echo -e "---------------------------------------------"
 echo -e "This script will attempt to start and install all system services necessary to run space."
@@ -42,7 +87,7 @@ else
 fi
 
 easy_install pip
-
+pip -r /srv/space/requirements.txt
 
 echo -n
 echo -e "$OK Adding MongoDB repo, your server is about to become #webscale $ENDC"
